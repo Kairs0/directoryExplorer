2bbcacb48ab4e3e2d66271f9f3919e186ec1ab9b
Kairs0 <arnaud.convers@gmail.com>
2018-04-22 09:00:51 +0200

    cleaning main

--- main.c
+++ main.c
@@ -9,30 +9,32 @@
 #include "listdir.h"
 #include "cmds.h"
 
+#define NO_PATH "No path provided."
+#define COND_SEC_INT "Second parameter must be positive integer."
+#define IO_ERR "The directory couldn't be opened."
+
 int main(int argc, char *argv[])
 {
-	struct lnode *a = NULL;
+    struct lnode *a = NULL;
 
-	if (argv[1] != NULL) {
-		if (argv[2] == NULL) {
-			a = createTreeLimited(argv[1],0);
-		} else {
-			int depth = atoi(argv[2]);
-			if (depth >= 0)
-				a = createTreeLimited(argv[1],depth);
-			else
-				printf("You have to put a positive integer for"
-					" the secound parameter.\n");
-			// if (depth == 0)
-			// 	printf("n");
-			// else
-			// 	a = createTreeLimited(argv[1],depth);
-		}
-	}	
+    if (argv[1] == NULL) {
+        printf("%s %s\n", NO_PATH, IO_ERR);
+        return -1;
+    }
 
-	if(a != NULL)
-		shell(a);
-	else
-		printf("The directory couldn't be opened.\n");
-	return 0;
+    if (argv[2] == NULL) {
+        a = createTreeLimited(argv[1], 0);
+    } else {
+        int depth = atoi(argv[2]);
+        if (depth < 0)
+            printf("%s\n", COND_SEC_INT);
+        else 
+            a = createTreeLimited(argv[1], depth);
+    }
+
+    if (a != NULL)
+        shell(a);
+    else
+        printf("%s\n", IO_ERR);
+    return 0;
 }
